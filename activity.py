dc51f1317417119d00442b0340d3262c8725f151
Bernat Brunet <bernat@nan-tic.com>
2024-06-03 19:04:04 +0200

    Add call transcription with chatGPT. Save voice file too.
    Fix some bugs:
    - When get the CDR list.
    - Fields names
    
    Add translates.

--- activity.py
+++ activity.py
@@ -2,11 +2,14 @@
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from datetime import timedelta
+from io import BytesIO
+from openai import OpenAI
 
 from trytond.model import ModelView, fields
 from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
 from trytond.pyson import Eval, Bool
+from trytond.config import config
 from trytond.modules.widgets import tools
 from trytond.i18n import gettext
 from trytond.exceptions import UserError
@@ -17,6 +20,7 @@ class Activity(metaclass=PoolMeta):
     __name__ = 'activity.activity'
 
     call_id = fields.Char('Call ID', readonly=True)
+    language = fields.Many2One('ir.lang', 'Language Call')
     call_transcription = fields.Text('Call Transcription',
         states={
             'invisible': ~Bool(Eval('call_id')),
@@ -38,9 +42,11 @@ class Activity(metaclass=PoolMeta):
         # TODO: Allow to download the record file at any time, not only
         # after the call.
         pool = Pool()
+        Configuration = pool.get('activity.configuration')
         User = pool.get('res.user')
         Attachment = pool.get('ir.attachment')
 
+        config = Configuration(1)
         user = User(Transaction().user)
         employee = user.employee if user else None
         if not employee:
@@ -58,7 +64,9 @@ class Activity(metaclass=PoolMeta):
         for activity in activities:
             if not activity.call_id:
                 continue
-            records_list = pbx.get_records_list(page='1', page_size='5',
+            page = config.yeastar_record_list_page or '1'
+            page_size = config.yeastar_record_list_page_size or '5'
+            records_list = pbx.get_records_list(page=page, page_size=page_size,
                 sort_by='id', order_by='desc')
             if not records_list or not records_list.get('data', None):
                 continue
@@ -88,7 +96,8 @@ class Activity(metaclass=PoolMeta):
             response = pbx._requests('GET',
                 args=record_info['download_resource_url'], token=token)
 
-            transcription = cls.speech_to_text(response)
+            lang = activity.language.code if activity.language else None
+            transcription = cls.speech_to_text(response, language=lang)
 
             activity.duration = (timedelta(seconds=duration)
                 if duration else timedelta(seconds=0))
@@ -110,9 +119,17 @@ class Activity(metaclass=PoolMeta):
             Attachment.save(attachments)
 
     @classmethod
-    def speech_to_text(cls, audio_file):
-        # TODO: Convert wav to text
-        # using google or use chatgpt ¿?
+    def speech_to_text(cls, audio_file, language=None):
         if not audio_file:
             return None
-        return 'TMP SPEECH TO TEXT'
+        client = OpenAI(api_key=config.get('openai', 'api_key'),
+            organization=config.get('openai', 'organization'))
+        audio = BytesIO(audio_file)
+        # It is important to set the filename otherwise the API will not
+        # recognize the audio
+        audio.name = 'file.wav'
+        if not language:
+            language = Transaction().context.get('language')
+        transcription = client.audio.transcriptions.create(model="whisper-1",
+            file=audio, language=language)
+        return transcription.text
