5db59839487fa48d7239a8a82055afb0867c198b
Albert Cervera i Areny <albert@nan-tic.com>
2025-08-05 10:43:40 +0200

    Add target field in operand of domains.

--- company.py
+++ company.py
@@ -12,7 +12,7 @@ class Employee(metaclass=PoolMeta):
 
     yeastar_pbx = fields.Many2One('yeastar.pbx', 'Yeastar PBX',
         domain=[
-            ('company', '=', Eval('company')),
+            ('company.id', '=', Eval('company')),
             ])
     yeastar_extension = fields.Integer('Yeastar Extension')
 
